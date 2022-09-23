3a9063a11e82de69fe1df92d745e8dfe65265b36
Chris Mac <hyer.chris@gmail.com>
2022-09-22 20:43:45 -0700

    ended with adding route to grab a person by id

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+];
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+    const currDate = new Date().toString(); 
+    response.send(`
+        <div> 
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${currDate}</p>
+        </div> 
+    `);
+});
+
+app.get('/api/persons/:id',(request,response) => {
+    const id = request.params.id ? Number(request.params.id) : null;
+   
+    const person = persons.find(person => person?.id === id);
+    
+    person ? response.json(person) 
+        : response.status(404).end(); 
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
